ba025287feb8396f7a3316056865d19cf0f41d32
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-03 19:14:16 +1000

    multidimensional arraylist

---
+++ DSAwithKUNAL/src/arraysAndArrayLists/MultiDemArrayList.java
@@ -0,0 +1,25 @@
+package arraysAndArrayLists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiDemArrayList {
+	public static void main(String[] args) {
+		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+		Scanner in = new Scanner(System.in);
+		// initialisation
+		for (int i = 0; i < 3; i++) {
+			list.add(new ArrayList<>());
+		}
+
+		// add elements
+		for (int j = 0; j < 3; j++) {
+			for (int k = 0; k < 3; k++) {
+				list.get(j).add(in.nextInt());
+			}
+		}
+
+		System.out.println(list);
+
+	}
+}
