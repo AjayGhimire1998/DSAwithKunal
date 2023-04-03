efb30aaf582e654aed3671f0737ec4cacd12ae2f
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-03 18:55:58 +1000

    tweeking arraylist methods

--- DSAwithKUNAL/src/arraysAndArrayLists/SampleArrayList.java
+++ DSAwithKUNAL/src/arraysAndArrayLists/SampleArrayList.java
@@ -14,15 +14,40 @@ public class SampleArrayList<T> {
 	}
 
 	public String toString() {
-		return String.format("[ size: %d ]", size);
+		return String.format("[ size: %d, contents: %s  ]", size, getValues());
+	}
+
+	public String getValues() {
+		String result = "";
+		for (int i = 0; i < list.size(); i++) {
+			result += list.get(i) + ",";
+		}
+		return result;
+	}
+
+	public void add(T a) {
+		list.add(a);
+	}
+
+	public boolean contains(T a) {
+		return list.contains(a);
+	}
+
+	public void set(int index, T t) {
+		list.set(index, t);
 	}
 
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList<>(10);
 		list.add(1);
 		System.out.println(list.size());
-		System.out.println(list.get(0));
+		System.out.println(list);
 		SampleArrayList<Character> chars = new SampleArrayList<>(10);
+		chars.add('a');
+		chars.add('c');
+		System.out.println(chars.toString());
+		System.out.println(chars.contains('a'));
+		chars.set(1, 'b');
 		System.out.println(chars.toString());
 	}
 
