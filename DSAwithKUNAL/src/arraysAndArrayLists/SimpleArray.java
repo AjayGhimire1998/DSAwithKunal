d26511cf2e9f77ee58cf4981a8dcc4534890578a
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-03 16:48:46 +1000

    by default int is zero, string is null

--- DSAwithKUNAL/src/arraysAndArrayLists/SimpleArray.java
+++ DSAwithKUNAL/src/arraysAndArrayLists/SimpleArray.java
@@ -3,5 +3,8 @@ package arraysAndArrayLists;
 public class SimpleArray {
 	public static void main(String[] args) {
 		int[] intArray;
+
+		intArray = new int[5];
+		System.out.println(intArray[0]);
 	}
 }
