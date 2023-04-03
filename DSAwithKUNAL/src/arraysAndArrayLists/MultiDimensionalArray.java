3c1ef9b229157d852e2050d4e0649d12bd624e8d
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-03 17:26:24 +1000

    method to display three D arays

--- DSAwithKUNAL/src/arraysAndArrayLists/MultiDimensionalArray.java
+++ DSAwithKUNAL/src/arraysAndArrayLists/MultiDimensionalArray.java
@@ -3,12 +3,16 @@ package arraysAndArrayLists;
 public class MultiDimensionalArray {
 	public static void main(String[] args) {
 		int[][] nums = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		int[][][] numsTwo = { { { 1, 1 }, { 2, 2 }, { 3, 3 } }, { { 4, 4 }, { 5, 5 }, { 6, 6 } },
+				{ { 7, 7 }, { 8, 8 }, { 9, 9 } } };
 //		System.out.println(Arrays.toString(nums));
-		toString(nums);
+		twoDArraystoString(nums);
+		threeDArraysToString(numsTwo);
 	}
 
-	static void toString(int[][] nums) {
+	static void twoDArraystoString(int[][] nums) {
 		System.out.print("[");
+
 		for (int i = 0; i < nums.length; i++) {
 			System.out.print("[");
 			for (int j = 0; j < nums[i].length; j++) {
@@ -24,6 +28,38 @@ public class MultiDimensionalArray {
 				System.out.print("], ");
 			}
 		}
-		System.out.println("]");
+
+		System.out.print("]");
+		System.out.println();
+	}
+
+	static void threeDArraysToString(int[][][] nums) {
+
+		System.out.print("[");
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print("[");
+			for (int j = 0; j < nums[i].length; j++) {
+				System.out.print("[");
+				for (int k = 0; k < nums[i][j].length; k++) {
+					if (k == nums[i][j].length - 1) {
+						System.out.print(nums[i][j][k]);
+					} else {
+						System.out.print(nums[i][j][k] + ",");
+					}
+				}
+				if (j == nums[i].length - 1) {
+					System.out.print("]");
+				} else {
+					System.out.print("], ");
+				}
+
+			}
+			if (i == nums.length - 1) {
+				System.out.print("]");
+			} else {
+				System.out.print("], ");
+			}
+		}
+		System.out.print("]");
 	}
 }
