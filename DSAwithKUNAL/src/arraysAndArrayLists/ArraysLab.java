57ff53e3203dfeb082b18ea4d85cc9fcc37c2e73
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-04 21:04:29 +1000

    now time to watch interstellar peacefully

--- DSAwithKUNAL/src/arraysAndArrayLists/ArraysLab.java
+++ DSAwithKUNAL/src/arraysAndArrayLists/ArraysLab.java
@@ -1,5 +1,6 @@
 package arraysAndArrayLists;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArraysLab {
@@ -10,6 +11,16 @@ public class ArraysLab {
 		System.out.println(Arrays.toString(chars));
 		reverse(chars);
 		System.out.println(Arrays.toString(chars));
+
+		ArrayList<Integer> nums = new ArrayList<>();
+		nums.add(1);
+		nums.add(8);
+		nums.add(2);
+		nums.add(9);
+		nums.add(0);
+		System.out.println(getMax(nums));
+		reverse(nums);
+		System.out.println(nums);
 
 	}
 
@@ -36,6 +47,25 @@ public class ArraysLab {
 			char temp = chars[i];
 			chars[i] = chars[chars.length - 1 - i];
 			chars[chars.length - 1 - i] = temp;
+
+		}
+	}
+
+	static int getMax(ArrayList<Integer> nums) {
+		int max = nums.get(0);
+		for (int i = 0; i < nums.size(); i++) {
+			if (nums.get(i) >= max) {
+				max = nums.get(i);
+			}
+		}
+		return max;
+	}
+
+	static void reverse(ArrayList<Integer> nums) {
+		for (int i = 0; i < nums.size() / 2; i++) {
+			int temp = nums.get(i);
+			nums.set(i, nums.get(nums.size() - 1 - i));
+			nums.set(nums.size() - 1 - i, temp);
 
 		}
 	}
