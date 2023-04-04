0349fdc3417fab98b36c64faa911329c94e0268e
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-04 20:38:27 +1000

    swapping values in an array

--- DSAwithKUNAL/src/arraysAndArrayLists/ArraysLab.java
+++ DSAwithKUNAL/src/arraysAndArrayLists/ArraysLab.java
@@ -1,9 +1,13 @@
 package arraysAndArrayLists;
 
+import java.util.Arrays;
+
 public class ArraysLab {
 	public static void main(String[] args) {
 		char[] chars = { 'a', 'c', 'b', 'y', 'w' };
 		System.out.println(getMax(chars));
+		swap(chars, 1, 4);
+		System.out.println(Arrays.toString(chars));
 	}
 
 	static char getMax(char[] chars) {
@@ -15,5 +19,12 @@ public class ArraysLab {
 		}
 
 		return max;
+	}
+
+	static void swap(char[] chars, int indexAt, int indexWith) {
+		char temp = chars[indexAt];
+		chars[indexAt] = chars[indexWith];
+		chars[indexWith] = temp;
+
 	}
 }
