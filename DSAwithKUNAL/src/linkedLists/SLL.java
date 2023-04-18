56a1b94e3135551f2351bce66e8e3fa78ee5556f
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-19 07:08:38 +1000

    singly ll again

--- DSAwithKUNAL/src/linkedLists/SLL.java
+++ DSAwithKUNAL/src/linkedLists/SLL.java
@@ -1,8 +1,43 @@
 package linkedLists;
 
-public class SLL {
+public class SLL<T> {
 	private static class Node<T> {
 		private T element;
 		private Node<T> next;
+
+		Node(T e) {
+			element = e;
+		}
+
+		Node(T e, Node<T> n) {
+			element = e;
+			next = n;
+		}
+
+		public T getElement() {
+			return element;
+		}
+
+		public Node<T> getNext() {
+			return next;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size;
+
+	SLL() {
+		size = 0;
+		head = null;
+		tail = null;
+	}
+
+	public Node<T> getHead() {
+		return head;
+	}
+
+	public Node<T> getTail() {
+		return tail;
 	}
 }
