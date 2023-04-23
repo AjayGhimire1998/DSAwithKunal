fe3540976999dde34dd2dfef8248fc8bfcc761ac
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-23 19:32:51 +1000

    adding in cll

--- DSAwithKUNAL/src/linkedLists/CLL.java
+++ DSAwithKUNAL/src/linkedLists/CLL.java
@@ -22,4 +22,45 @@ public class CLL<T> {
 			next = n;
 		}
 	}
+
+	private Node<T> head = null;
+	private Node<T> tail = null;
+	private int size = 0;
+
+	CLL() {
+
+	}
+
+	public T getHead() {
+		if (isEmpty()) {
+			return null;
+		}
+		return head.getElement();
+	}
+
+	public T getTail() {
+		if (isEmpty()) {
+			return null;
+		}
+		return tail.getElement();
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void insert(T elem) {
+		Node<T> newNode = new Node<T>(elem, null);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+			return;
+		}
+	}
+
 }
