5cce4af3ebb6e46d68ed235685b7f25c8cc44bd7
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-23 19:27:25 +1000

    circular LL

---
+++ DSAwithKUNAL/src/linkedLists/CLL.java
@@ -0,0 +1,25 @@
+package linkedLists;
+
+public class CLL<T> {
+	private static class Node<T> {
+		private T element;
+		private Node<T> next;
+
+		Node(T e, Node<T> n) {
+			element = e;
+			next = n;
+		}
+
+		T getElement() {
+			return element;
+		}
+
+		Node<T> getNext() {
+			return next;
+		}
+
+		void setNext(Node<T> n) {
+			next = n;
+		}
+	}
+}
